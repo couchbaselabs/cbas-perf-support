55006caf92a827fff33dbd6e503b6b76fcfda40b
Hussain Towaileb <Hussain.Towaileb@Couchbase.com>
2021-03-18 03:56:05 +0300

    Generate random alphanumeric characters data
    Change-Id: I77c94516b838a132202230ac23c9d594a74eebb4
    Reviewed-on: http://review.couchbase.org/c/cbas-perf-support/+/148891
    Tested-by: Hussain Towaileb <Hussain.Towaileb@Couchbase.com>
    Reviewed-by: Hussain Towaileb <Hussain.Towaileb@Couchbase.com>
    Reviewed-by: Till Westmann <till@couchbase.com>

--- json-data-generator/src/jsondatagen.py
+++ json-data-generator/src/jsondatagen.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 import sys
+import random
+import string
+
+
+# generate random alphanumeric data for each field
+def generate_random_text(size, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 # convert the file size to readable format
@@ -44,13 +51,14 @@ print("Last field will be ", format_bytes(eachFieldSize + lastFieldLeftOver))
 
 # Create the JSON document as per the provided input
 print("Generating record started...")
-eachFieldValue = "a" * eachFieldSize
-lastFieldValue = ("a" * eachFieldSize) + ("a" * lastFieldLeftOver)
-jsonObjectStr = "{"
-for fieldNumber in range(recordFieldsCount - 1):
-    jsonObjectStr += "\"f" + str(fieldNumber) + "\":\"" + eachFieldValue + "\","
-jsonObjectStr += "\"f" + str(recordFieldsCount - 1) + "\":\"" + lastFieldValue + "\"}\n"
-print("Record creation completed. Each record size is: ", format_bytes(len(jsonObjectStr)))
+jsonData = ""
+for recordNumber in range(numberOfRecordsPerFile):
+    jsonData += "{"
+    for fieldNumber in range(recordFieldsCount - 1):
+        jsonData += "\"f" + str(fieldNumber) + "\":\"" + generate_random_text(eachFieldSize) + "\","
+    jsonData += "\"f" + str(recordFieldsCount - 1) + "\":\"" + generate_random_text(eachFieldSize) + \
+                generate_random_text(lastFieldLeftOver) + "\"}\n"
+print("Record creation completed. Each record size is: ", format_bytes(len(jsonData)))
 print("----------------------------------------------------------------------------\n")
 
 
@@ -61,10 +69,9 @@ numberOfFiles = int(totalNumberOfRecords / numberOfRecordsPerFile)
 print("Number of files to be generated: ", numberOfFiles)
 print("Number of records per file: ", numberOfRecordsPerFile)
 print("Number of records in last file: ", numberOfRecordsPerFile + leftOverRecords)
-print("Total size per file: ", format_bytes(numberOfRecordsPerFile * len(jsonObjectStr)))
-print("Actual total data size: ", format_bytes(totalNumberOfRecords * len(jsonObjectStr)))
+print("Total size per file: ", format_bytes(len(jsonData)))
+print("Actual total data size: ", format_bytes(numberOfFiles * len(jsonData)))
 
-finalResult = jsonObjectStr * numberOfRecordsPerFile
 file = open(fileName, "w")
-file.write(finalResult)
+file.write(jsonData)
 file.close()
